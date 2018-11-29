bef9faf59324218421b749302977bed2b90d7088
XRafe <nikita-gorin98@yandex.ru>
2018-11-29 14:18:54 +0300

    ch

--- Lab1Var5/src/Lab2/Panel.java
+++ Lab1Var5/src/Lab2/Panel.java
@@ -5,39 +5,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel extends JPanel {
-	
+	Ship Ship = new Ship();
 	
 	public void paint(Graphics g) {
 		super.paint(g);
-
-		 int yline = -5;
-
-
-		 if (FormShip.DrawShip) {
-        	 g.setColor(Color.GREEN);
-             g.fillRect(Ship._startPosX + 80, Ship._startPosY - 6, 120, 40);
-             g.setColor(Ship.MainColor);
-             g.fillRect(Ship._startPosX + 90, Ship._startPosY - 26, 90, 20);
-             g.setColor(Color.RED);
-             g.fillRect(Ship._startPosX + 110, Ship._startPosY - 56, 30, 30);
-             g.setColor(Color.GREEN);
-             g.drawLine(Ship._startPosX + 80, Ship._startPosY + 30, Ship._startPosX + 30, Ship._startPosY - 30);
-             while (yline < 35)
-             {
-                 g.drawLine(Ship._startPosX + 80, Ship._startPosY + yline, Ship._startPosX + 30, Ship._startPosY - 30);
-
-                 g.drawLine(Ship._startPosX + 200, Ship._startPosY + yline, Ship._startPosX + 250, Ship._startPosY - 30);
-                 yline++;
-             }
-
-             g.drawLine(Ship._startPosX + 200, Ship._startPosY + 30, Ship._startPosX + 250, Ship._startPosY - 30);
-
-		 }
-         
-
-            
-		
-	
-         
+		Ship.DrawShip(g);
 	}
 }
--- Lab1Var5/src/Lab2/Ship.java
+++ Lab1Var5/src/Lab2/Ship.java
@@ -1,6 +1,8 @@
 package Lab2;
 
 import java.awt.Color;
+import java.awt.Graphics;
+
 
 
 
@@ -39,7 +41,11 @@ import java.awt.Color;
 
         public Color DopColor;
 
-
+        public Ship()
+        {
+        	
+        }
+        
         public Ship(int maxSpeed, float weight, Color mainColor, Color dopColor)
         {
             MaxSpeed = maxSpeed;
@@ -94,6 +100,38 @@ import java.awt.Color;
             }
         }
 
+        public void DrawShip(Graphics g) {
+
+   		 int yline = -5;
+
+
+   		 if (FormShip.DrawShip) {
+           	 g.setColor(Color.GREEN);
+                g.fillRect(Ship._startPosX + 80, Ship._startPosY - 6, 120, 40);
+                g.setColor(Ship.MainColor);
+                g.fillRect(Ship._startPosX + 90, Ship._startPosY - 26, 90, 20);
+                g.setColor(Color.RED);
+                g.fillRect(Ship._startPosX + 110, Ship._startPosY - 56, 30, 30);
+                g.setColor(Color.GREEN);
+                g.drawLine(Ship._startPosX + 80, Ship._startPosY + 30, Ship._startPosX + 30, Ship._startPosY - 30);
+                while (yline < 35)
+                {
+                    g.drawLine(Ship._startPosX + 80, Ship._startPosY + yline, Ship._startPosX + 30, Ship._startPosY - 30);
+
+                    g.drawLine(Ship._startPosX + 200, Ship._startPosY + yline, Ship._startPosX + 250, Ship._startPosY - 30);
+                    yline++;
+                }
+
+                g.drawLine(Ship._startPosX + 200, Ship._startPosY + 30, Ship._startPosX + 250, Ship._startPosY - 30);
+
+   		 }
+            
+
+               
+   		
+   	
+            
+   	}
 
      }
     
